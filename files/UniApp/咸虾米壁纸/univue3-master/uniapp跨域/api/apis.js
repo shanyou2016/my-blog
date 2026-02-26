8519781ae3ac934dea456c7a6b9ea6805a52f629
shanyou2016 <631797337@qq.com>
2026-02-26 18:05:49 +0800

    更新文件列表

--- files/UniApp/咸虾米壁纸/univue3-master/uniapp跨域/api/apis.js
+++
@@ -1,8 +0,0 @@
-import {request} from "@/utils/request.js"
-
-export function apiNbaData(){
-	return request({
-		url:"/api/match/playerranking/match/NBA/tabId/60"		
-	})	
-}
-
--- files/UniApp/咸虾米壁纸/univue3-master/uniapp跨域/utils/common.js
+++
@@ -1,9 +0,0 @@
-import {API_HOST,API_PROXY} from "../config.js"
-
-/**
- * 组装接口url
- */
-export const packApiUrl = (url = '') => {
-  if (url.slice(0, 4) === 'http') return url
-  else return `${API_HOST}${API_PROXY}${url}`
-}
